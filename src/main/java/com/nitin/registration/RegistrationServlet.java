2f82b81fb33e48dca57e534ad1218926ce5dbe02
Nitin Mudgal <nitinmudgal7@gmail.com>
2022-10-09 23:43:00 +0530

    commit 1
    commit 1

---
+++ src/main/java/com/nitin/registration/RegistrationServlet.java
@@ -0,0 +1,34 @@
+package com.nitin.registration;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class Registration
+ */
+@WebServlet("/register")
+public class RegistrationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String uname = request.getParameter("name");
+		String uemail = request.getParameter("email");
+		String upwd = request.getParameter("pass");
+		String umobile = request.getParameter("contact");
+		
+		PrintWriter out = response.getWriter();
+		out.print(uname);
+		out.print(uemail);
+		out.print(upwd);
+		out.print(umobile);
+		
+	}
+
+}
